053ff12b4b78aefeaa6f82e074a9f81454908f0b
ckem1st997 <hopxc1997@gmail.com>
2023-09-01 16:23:05 +0700

    update

--- rasa-setup/start_rasa.sh
+++ rasa-setup/start_rasa.sh
@@ -5,4 +5,7 @@ rasa train
 rasa train --data /app/models/data
 
 # Chạy máy chủ API Rasa với tùy chọn --enable-api và --debug
-rasa run --enable-api --debug
+# rasa run --enable-api --debug
+
+# rasa run --enable-api --debug
+# rasa run actions
